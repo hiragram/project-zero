77cfce474e0fdb2dfa03c297bea29de780eeaa0b
hiragram <y@yura.me>
2020-08-06 23:04:06 +0900

    Add UDPCommunication

---
+++ Assets/Network/UDPCommunication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class UDPCommunication {
+
+    private string hostName;
+    private int remotePort;
+
+    private int localPort;
+
+    public UDPCommunication(string hostName, int remotePort, int localPort) {
+        this.hostName = hostName;
+        this.remotePort = remotePort;
+        this.localPort = localPort;
+    }
+
+    public UdpClient MakeReceiverClient() {
+        var address = IPAddress.Parse("127.0.0.1");
+        var endpoint = new IPEndPoint(address, localPort);
+        var client = new UdpClient(endpoint);
+
+        return client;
+    }
+}
---
+++ Assets/Tests/NetworkTests/UDPCommunicationTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class UDPCommunicationTests {
+        [Test]
+        public void CanMakeReceiverConnection() {
+            var o = new UDPCommunication("other.local", 35001, 35001);
+            var client = o.MakeReceiverClient();
+
+            Assert.IsNotNull(client);
+        }
+    }
+}
