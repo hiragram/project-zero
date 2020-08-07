c08a1e11b8fe433288b7e4d92309a24494459c65
hiragram <y@yura.me>
2020-08-08 06:36:58 +0900

    Revert "Use Rigidbody"
    This reverts commit 56b932d0833b9dbcc23b234c37ab9dcf7628565e.

--- Assets/GuitarPlay/EquipGuitar.cs
+++ Assets/GuitarPlay/EquipGuitar.cs
@@ -26,16 +26,15 @@ public class EquipGuitar: MonoBehaviour {
         {
             var boneLimits = humanoidDesc.Description.human;
             var hip = FindTarget(boneLimits, HumanBodyBones.Hips);
-            var cube = AddTargetCube(hip);
-            cube.name = "ConnectionPointBodyA";
-            cube.transform.position = new Vector3(
-                cube.transform.position.x,
-                cube.transform.position.y + 0.04f,
-                cube.transform.position.z + 0.2f
-            );
-            var rigidBody = cube.AddComponent<Rigidbody>();
-            rigidBody.useGravity = false;
-            bodySideConnectionPoint = cube;
+            // var cube = AddTargetCube(hip);
+            // cube.name = "ConnectionPointBodyA";
+            // cube.transform.position = new Vector3(
+            //     cube.transform.position.x,
+            //     cube.transform.position.y + 0.04f,
+            //     cube.transform.position.z + 0.2f
+            // );
+            // bodySideConnectionPoint = cube;
+            bodySideConnectionPoint = hip.gameObject;
         }
         Assert.IsNotNull(bodySideConnectionPoint);
 
@@ -56,9 +55,16 @@ public class EquipGuitar: MonoBehaviour {
         }
         Assert.IsNotNull(guitarSideConnectionPoint);
 
-        var fixedJoint = bodySideConnectionPoint.AddComponent<FixedJoint>();
-        fixedJoint.connectedBody = guitarSideConnectionPoint.GetComponent<Rigidbody>();
-        fixedJoint.connectedMassScale = 0;
+        var constraintSource = new ConstraintSource();
+        constraintSource.sourceTransform = bodySideConnectionPoint.transform;
+        constraintSource.weight = 1;
+        var positionConstraint = guitarSideConnectionPoint.AddComponent<ParentConstraint>();
+        {
+            positionConstraint.AddSource(constraintSource);
+            positionConstraint.SetRotationOffset(0, new Vector3(-30, -90, 0));
+            positionConstraint.SetTranslationOffset(0, new Vector3(-0.1f, 0.1f, 0));
+        }
+        positionConstraint.constraintActive = true;
     }
 
     private Transform FindTarget(BoneLimit[] boneLimits, HumanBodyBones bone) {
